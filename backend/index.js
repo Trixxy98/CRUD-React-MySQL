41b6cfadedc2d99c4299c50fc0a927d3beecc2a0
Harith Fakrullah <rith@Riths-MacBook-Air.local>
2026-05-16 12:51:32 +0800

    first commit

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+import express from 'express'; //express ialah web framework yang digunakan bersama Node.js untuk membina aplikasi backend dan API dengan lebih mudah dan tersusun.
+import mysql2 from 'mysql2';
+
+const app = express();
+const db = mysql2.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root1234",
+    database: "test"
+})
+
+app.use(express.json()); //express.json() ialah middleware yang digunakan untuk memproses data yang dikirimkan dari client ke server.
+
+app.get("/", (req,res)=>{
+    res.json("Hello World");
+})
+
+app.get("/books", (req,res)=>{
+    const q = "SELECT * FROM books";
+    db.query(q, (err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data);
+    })
+})
+
+app.post("/books", (req,res)=>{
+    const q = "INSERT INTO books (`title`, `decs`,`cover`) VALUES (?)";
+    const values = 
+    [req.body.title, 
+        req.body.decs, 
+        req.body.cover];
+    db.query(q, [values], (err,data)=>{
+        if(err) return res.json(err)
+        return res.json("Books has been created successfully");
+    })
+})
+
+app.listen(8800, ()=>  {
+    console.log("Connected to backend!");
+})
